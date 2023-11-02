fb241a8db875f183d7258000b882c8d096bc7b52
firdawse <firdawse.guerbouzi1@gmail.com>
2023-11-02 23:59:43 +0000

    task2

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - add a node in the right of the parent
+ * @parent: parent of the specified  node
+ * @value: value of the node
+ * Return: new node or NULL if it fail
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->left;
+		parent->right->parent = new_node;
+	}
+	parent->right = new_node;
+	return (new_node);
+}
