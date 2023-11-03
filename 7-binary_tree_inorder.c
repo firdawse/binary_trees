8ef025c99035012fe4442a77149ca3b706f2f2be
firdawse <firdawse.guerbouzi1@gmail.com>
2023-11-03 13:25:58 -0700

    task7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,9 +1,8 @@
 #include "binary_trees.h"
 /**
  * binary_tree_inorder - print elements of tree in in-order traversal
- * @tree: tree 
+ * @tree: tree
  * @func: function to use
- * Return: Nothing
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
@@ -13,9 +12,8 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	}
 	else
 	{
-		
 		binary_tree_inorder(tree->left, func);
-        func(tree->n);
+		func(tree->n);
 		binary_tree_inorder(tree->right, func);
 	}
 }
