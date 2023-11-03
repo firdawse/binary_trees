decd36789d4b94e99d56ef9863aa06eb04c84340
firdawse <firdawse.guerbouzi1@gmail.com>
2023-11-03 13:31:42 -0700

    task8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - print elements of tree in post-order traversal
+ * @tree: tree
+ * @func: function to use
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	else
+	{
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
+	}
+	func(tree->n);
+}
